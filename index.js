e71b0b7be02083a4f0945a45005bfd83499b6581
Jack Williams <jackmwilliams35@gmail.com>
2023-09-25 15:33:57 -0500

    added images to json and some JS pseudo

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+// Resources
+//   http://localhost:3000/Exercises
 
+//   Home
+//   http://localhost:3000
+
+//pseudo code:
+// use get command for json
+// iterate through the objects in db.json file
+// 
+
+const exerciseUrl = "http://localhost:3000/Exercises"
+
+fetch (exerciseUrl)
+    .then()
